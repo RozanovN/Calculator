995dc8e52f60741d6b3d096d01da1f38077a7568
OceNick <ozcenick@gmail.com>
2021-12-12 13:57:38 -0800

    Implement input processing function in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        processInput();
     }
+
+    public static void processInput() {
+        Scanner input = new Scanner(System.in);
+        boolean isFinished = false;
+        System.out.println("Please, chose the type of calculator from following Algebra, Basic Math, Calculus," +
+                " Linear Algebra :");
+        String userInput = input.nextLine();
+        while (!isFinished) {
+            if (userInput.equals("q") || userInput.equals("quit")) {
+                isFinished = true;
+            }
+            if (verifyInput(userInput, "calculator type")) {
+                calulator(userInput);
+            } else{
+                System.out.println(userInput + "is not valid type of calculator. Please, try again:");
+            }
+        }
+    }
+
 
 }
