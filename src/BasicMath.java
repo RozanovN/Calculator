3d28b62b8eb28336c4de2666714fb5ab957f0462
OceNick <ozcenick@gmail.com>
2021-12-12 23:30:23 -0800

    Implement a function to determine the order of operations

--- src/BasicMath.java
+++ src/BasicMath.java
@@ -5,5 +5,30 @@ import java.util.Collections;
 
 public class BasicMath {
 
+    public BasicMath() {
+    }
 
+    public int calculation(String[] listOfOperations) {
+
+    }
+
+    public static ArrayList<Integer> determineOrderOfOperation(String[] listOfOperations) {
+        Map<Character, Integer> operationOrder = Map.ofEntries(
+                entry('(', 3),
+                entry('/', 2),
+                entry('*', 2),
+                entry('+', 1),
+                entry('-', 1)
+        );
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (String operation : listOfOperations) {
+            // Add
+            result.add(operationOrder.get(operation.charAt(0)));
+        }
+        return result;
+    }
+
+    pablic static void sortInDescendingOrder(ArrayList<?> listToSort) {
+
+    }
 }
