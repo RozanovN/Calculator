c9635abf06d4e9375b0caa2eb7061b3e9838e8b7
OceNick <ozcenick@gmail.com>
2021-12-26 20:02:27 -0800

    Add GUI declaration to Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,46 +5,7 @@ import static java.util.Map.entry;
 public class Main {
 
     public static void main(String[] args) {
-        processInput();
+        GUI calculator = new GUI();
     }
 
-    public static void processInput() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Please, chose the type of calculator from following Algebra, Basic Math, Calculus," +
-                " Linear Algebra :");
-        String userInput = input.nextLine();
-        boolean isFinished = false;
-        while (!isFinished) {
-            if (userInput.equals("q") || userInput.equals("quit")) {
-                isFinished = true;
-            }
-            else if (verifyInput(userInput, "calculator type")) {
-                calculator(userInput);
-                System.out.println("If you no longer need CalcApp, please, enter <q> or <quit>. If you want to change" +
-                        "the type of calculator, please, choose from following Algebra, Basic Math, Calculus, " +
-                        "Linear Algebra:");
-                userInput = input.nextLine();
-            } else{
-                System.out.println(userInput + " is not valid type of calculator. Please, try again:");
-            }
-        }
-    }
-
-    public static boolean verifyInput(String userInput, String typeOfVerifcation) {
-        // type of input will be implemented later
-        String[] typesOfCalculators = {"Algebra", "Basic Math", "Calculus", "Linear Algebra"};
-        for (String type : typesOfCalculators) {
-            if (type.equals(userInput)){
-                return true;
-            }
-        }
-        return false;
-    }
-
-//    public static void calculator(String typeOfCalculator){
-//        Map<String, Class<?>> calc = Map.ofEntries(
-//                entry("Algebra", Algebra.class),
-//                entry("c", "d")
-//        );
-//    }
 }
