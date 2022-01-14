b29e2f407865a2a0b8c4a7a83f4afc2025f32814
OceNick <ozcenick@gmail.com>
2022-01-14 13:37:34 -0800

    Add documentation

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
-public class Main {
-
-    public static void main(String[] args) {
+/**
+ *
+ * Main class.
+ *
+ * @author Nikolay Rozanov
+ * @version 2022
+ */
+public final class Main {
+    private Main() {
+    }
+    /**
+     * Initiates GUI of a calculator.
+     *
+     * @param args command line arguments (unused)
+     */
+    public static void main(final String[] args) {
         GUI calculator = new GUI();
     }
-
 }
