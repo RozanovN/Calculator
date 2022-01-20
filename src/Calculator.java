8b512a001f382551148b6cb2cb42629dc6b50b19
OceNick <ozcenick@gmail.com>
2022-01-20 15:23:56 -0800

    Add calculator class

---
+++ src/Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+    private String[] BUTTON_VALUES;
+
+    public String[] getButtonValues() {
+        return BUTTON_VALUES;
+    }
+
+    public void setButtonValues(final String[] buttonValues) {
+        this.BUTTON_VALUES = buttonValues;
+    }
+}
